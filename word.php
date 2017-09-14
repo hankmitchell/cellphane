a0fc940ca094c3fb5759d328ebdc43a1f173ddd6
hank.mitchell <hank.mitchell@gmail.com>
2017-09-14 16:58:43 -0400

    v2.2

--- word.php
+++ word.php
@@ -1,15 +1,14 @@
 <?php
-require_once( "../lib/smartypants.php" );
-$loc = '//' . $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ];
-if ( preg_match( '/leadsaber\.com\/mailstripe/', $loc ) ) {
-	header( "Location: http://mailstripe.net/build/" );
-	exit;
+require_once("../lib/smartypants.php");
+$loc = '//' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+if (preg_match('/leadsaber\.com\/mailstripe/', $loc)) {
+    header("Location: http://mailstripe.net/build/");
+    exit;
 }
 
-foreach ( $_GET as $key => $value ) {
-	$$key = stripslashes( trim( $value ) );
+foreach ($_GET as $key => $value) {
+    $$key = stripslashes(trim($value));
 }
-foreach ( $_POST as $key => $value ) {
-	$$key = stripslashes( trim( $value ) );
+foreach ($_POST as $key => $value) {
+    $$key = stripslashes(trim($value));
 }
-
