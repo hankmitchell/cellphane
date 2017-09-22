be31f784342288c3bb35dbc164bbeb0d07fa440a
hank.mitchell <hank.mitchell@gmail.com>
2017-09-22 12:13:35 -0400

    testy

--- word.php
+++ word.php
@@ -1,10 +1,7 @@
 <?php
 require_once("../lib/smartypants.php");
 $loc = '//' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-if (preg_match('/leadsaber\.com\/mailstripe/', $loc)) {
-    header("Location: http://mailstripe.net/build/");
-    exit;
-}
+
 
 foreach ($_GET as $key => $value) {
     $$key = stripslashes(trim($value));
