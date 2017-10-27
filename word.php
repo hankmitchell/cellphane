c56aa16925d047b8965649b62dd4a1ac45935d4e
hank.mitchell <hank.mitchell@gmail.com>
2017-10-27 08:32:45 -0400

    post loop

--- word.php
+++ word.php
@@ -5,7 +5,9 @@ $loc = '//' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 foreach ($_GET as $key => $value) {
     $$key = stripslashes(trim($value));
+    $_GET[$key] = $value;
 }
 foreach ($_POST as $key => $value) {
     $$key = stripslashes(trim($value));
+    $_POST[$key] = $value;
 }
